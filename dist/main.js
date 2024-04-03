efa7959d59af0a979b70cfb99606b784386caeb2
Mack <makiraicevic@gmail.com>
2024-04-02 17:33:05 -0700

    testing the live server

--- dist/main.js
+++ dist/main.js
@@ -9,24 +9,52 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/sass/main.scss":
+/*!****************************!*\
+  !*** ./src/sass/main.scss ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://webpack-starter/./src/sass/main.scss?");
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
 /***/ (() => {
 
-eval("console.log('Index.js 123...');\r\n\r\nconsole.log('Here again ....');\r\n\n\n//# sourceURL=webpack://webpack-starter/./src/index.js?");
+eval("\r\nconsole.log('Index.js 123...');\r\n\r\nconsole.log('Here agn ....');\r\n\r\nlet title = document.querySelector('h1');\r\n\r\ntitle.style.fontWeight = '900';\r\n\n\n//# sourceURL=webpack://webpack-starter/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	__webpack_modules__["./src/index.js"](0, {}, __webpack_require__);
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	__webpack_modules__["./src/sass/main.scss"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-// import './sass/main.scss';
 
 console.log('Index.js 123...');
 
 console.log('Here agn ....');
+
+let title = document.querySelector('h1');
+
+title.style.fontWeight = '900';
