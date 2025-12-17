599f71bc8fda7811190ac3d751d85cea8f49283b
Mack Raicevic <makiraicevic@gmail.com>
2025-12-17 15:43:50 +0100

    made wp css build as an option

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,85 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const HtmlLoader = require('html-loader');
+const webpack = require('webpack');
+const wpThemeHeader = require('./wp-css-build/wp-theme-header');
+
 
 const indexJs = path.join(__dirname, 'src/index.js');
 const mainCSS = path.join(__dirname, 'src/sass/main.scss');
 
-module.exports = {
-    mode: 'development',
-    watch: true,
-    entry: {
-        main: './src/index.js'
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true, // Cleans the dist folder before each build
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, './dist'),
-        },
-        watchFiles: ['src/**/*.html'], // 👈 watch HTML templates
-        liveReload: true,
-        hot: true,
-        open: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: ['html-loader']
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
-                    // { loader: 'css-loader', options: { sourceMap: true } }, 
-                    // { loader: 'sass-loader', options: { sourceMap: true } },
-                // { 
-                //     loader: 'css-loader', options: { sourceMap: true } },
-                // { 
-                //     loader: 'postcss-loader', options: { sourceMap: true } },
-                // { 
-                //     loader: 'sass-loader', options: { sourceMap: true } },
-            },
-        ],
-    },
-    plugins: [
-         new HtmlWebpackPlugin({
-            template: './src/index.html', // your source HTML
-            filename: 'index.html',       // output file in dist/
-            inject: 'body', // or 'head', if you want it in <head>
-            chunks: ['main'], // only include the 'main' chunk
+module.exports = (env = {}) => {
+    const isWordPress = env.wordpress === 'true' || env.wordpress === true;
+
+
+    const plugins = [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body',
+            chunks: ['main'],
         }),
         new MiniCssExtractPlugin({
-            filename: '[name].css',
+            filename: '[name].min.css',
         })
-    ]
+    ];
+
+    if(isWordPress) {
+            plugins.push(
+                new webpack.BannerPlugin({
+                    banner: wpThemeHeader,
+                    raw: true,               //  don't wrap in /* */
+                    test: /\.css$/i,         //  apply ONLY to CSS
+                })
+            );
+        }
+
+    return {
+        mode: 'development',
+        watch: (isWordPress) ? false : true,
+
+        entry: {
+            main: './src/index.js',
+        },
+
+        output: {
+            filename: 'index.js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true, // Cleans the dist folder before each build
+        },
+
+        devServer: {
+            static: {
+                directory: path.join(__dirname, './dist'),
+            },
+            watchFiles: ['src/**/*.html'], // watch HTML templates
+            liveReload: true,
+            hot: true,
+            open: true,
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    use: ['html-loader']
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+                    // { loader: 'css-loader', options: { sourceMap: true } }, 
+                    // { loader: 'sass-loader', options: { sourceMap: true } },
+                    // { 
+                    //     loader: 'css-loader', options: { sourceMap: true } },
+                    // { 
+                    //     loader: 'postcss-loader', options: { sourceMap: true } },
+                    // { 
+                    //     loader: 'sass-loader', options: { sourceMap: true } },
+                },
+            ],
+        },
+        
+        plugins
+        
+    };
 };
---
+++ wp-css-build/wp-theme-header.js
@@ -0,0 +1,14 @@
+module.exports = `
+/*
+Theme Name: MaivDigital
+Theme URI: https://maivdigital.com/
+Author: MaivDigital.com
+Author URI: https://maivdigital.com/
+Description: MaivDigital Theme made custom by our team. Responsive, bootstrap, animate.css, Font Awesome Support
+Version: 3.0.0
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: maivdigital
+Tags: one-column, two-columns, three-columns
+*/
+`;
